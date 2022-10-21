da7d822ff507b10da932509e2c19cbec1a9a2e46
DeeppriyaGupta <113301249+DeeppriyaGupta@users.noreply.github.com>
2022-10-21 18:43:03 +0530

    Update whatsappmsg.py

--- whatsappmsg.py
+++ whatsappmsg.py
@@ -4,7 +4,7 @@ import os
 # Default is "127.0.0.1" and 5037
 client = AdbClient(host="127.0.0.1", port=5037)
 time.sleep(1)
-phone=["8871991181"]
+phone=["7523857255"]
 for  i in phone:
     client.devices()[0].shell(f'am start -a android.intent.action.VIEW -d "https://api.whatsapp.com/send?phone=+918871991181"')
     time.sleep(1)
